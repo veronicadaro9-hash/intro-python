4cb3d3d2b639694a88a13e446450040c0fa774c5
Verónica Daza Romero <veronica@MacBook-Air-de-Veronica.local>
2026-09-25 10:05:14 +0200

    repaso bucle

---
+++ bucles_lista.py
@@ -0,0 +1,14 @@
+# contendor, lista, array
+lista_all=["Maria",10,True,3.99,[1,2,3]]
+'''
+print(len(lista_all))#obtener la longitud de lista
+print(lista_all[0])#obtener la primera posicion
+print(lista_all[len(lista_all)-1])#obtener la ultima posicion
+for i in lista_all:
+    print(i)
+'''
+print( lista_all[len(lista_all)-1][1])
+'''
+for i in range( 0, len(lista_all) ):
+    print(lista_all[i])
+'''    
